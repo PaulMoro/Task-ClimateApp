f1239b5ea10f3b2af9d60ba45dfd344c6fa6e958
Paul Montoya <paulmontoya15@gmail.com>
2021-03-09 13:49:19 -0500

    cll to api was extends, because I chonge the key api

--- src/components/Future.js
+++ src/components/Future.js
@@ -50,7 +50,7 @@ const Future = () => {
     });
     useEffect(async () => {
       try {
-        var res = await request.get('https://api.openweathermap.org/data/2.5/weather?q=bogota&units=metric&appid=bb2f48b74892dd07194b9c119ce1ff43')
+        var res = await request.get('https://api.openweathermap.org/data/2.5/weather?q=bogota&units=metric&appid=https://api.openweathermap.org/data/2.5/weather?zip=01001,us&units=metric&appid=a967ec6d970677bf478e5f73d840390c')
         var api = res.body;
       } catch (err) {
         console.error(err);
